35d4a310f8aa39cf432c4234a5bc55ac3e1c2ad8
Patrick <prateekb1912@gmail.com>
2022-03-07 16:23:35 +0530

    Restart the project

--- api/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, make_response, jsonify, current_app
-from flask_sqlalchemy import SQLAlchemy
-
-main_api_bp = Blueprint('team_rankings_api', __name__)
-
-@main_api_bp.route('status')
-def status_service():
-    data = {'status': True}
-    return make_response(jsonify(data), 200)
--- main.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    from core.app import create_app
-
-    app = create_app()
-
-    app.run()
