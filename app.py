81964d3b3474ddd921a81640bad4bef0f2defb57
Patrick <prateekb1912@gmail.com>
2022-03-16 22:08:09 +0530

    Add config support

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    return "ICC Rankings API v 2.1"
+    secret_key = app.config.get("SECRET_KEY")
+    return f"This is a secret key {secret_key}"
