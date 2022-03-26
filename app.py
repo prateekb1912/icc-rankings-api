0d5499d08d52a063ee9950386d490e98a372f563
Patrick <prateekb1912@gmail.com>
2022-03-26 14:37:26 +0530

    Create app outside of function

--- app.py
+++ app.py
@@ -1,29 +1,24 @@
 import config
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from views import api_bp
 
-def create_app():
-    app = Flask(__name__)
-    app.config['JSON_SORT_KEYS'] = False
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
-    app.config['FLASK_DEBUG'] = 1
-    app.config['SQLALCHEMY_DATABASE_URI'] = config.Config.SQLALCHEMY_DATABASE_URI
+app.config['FLASK_DEBUG'] = 1
+app.config['SQLALCHEMY_DATABASE_URI'] = config.Config.SQLALCHEMY_DATABASE_URI
 
-    print(config.Config.SQLALCHEMY_DATABASE_URI)
+print(config.Config.SQLALCHEMY_DATABASE_URI)
 
-    print("DB TO BE INIT")
-    db = SQLAlchemy()
-    db.init_app(app)
-    print("DB CONFIG")
+print("DB TO BE INIT")
+db = SQLAlchemy()
+db.init_app(app)
+print("DB CONFIG")
 
-    app.register_blueprint(api_bp, url_prefix = f'/{config.Config.CURRENT_VERSION_API}')
-    print("API BP REGI")
+app.register_blueprint(api_bp, url_prefix = f'/{config.Config.CURRENT_VERSION_API}')
+print("API BP REGI")
 
-    return app
 
 if __name__ == '__main__':
-    print("HOHOYE CREATE APP")
-    app = create_app()
     app.run(debug=True)
