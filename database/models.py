4ae193a2a64a3820b083ea8bda1192aacd9338a6
Patrick <prateekb1912@gmail.com>
2022-03-26 22:27:54 +0530

    Add metdata base for tables

--- database/models.py
+++ database/models.py
@@ -1,6 +1,32 @@
-from sqlalchemy import (
-    Column, String, Integer, Enum
-)
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy import Column, DateTime, String, Enum, Integer
+from sqlalchemy.sql import func
+
+import re
+
+class IDSModelBase(object):
+    """
+    custom base class model.
+    """
+
+    @declared_attr
+    def __tablename__(cls):
+        return re.sub(r'(?<!^)(?=[A-Z])', '_', cls.__name__).lower()
+
+    created = Column(DateTime, server_default=func.now(),
+                     comment="The creation time\
+                              of the entry")
+    modified = Column(DateTime, server_default=func.now(),
+                      comment="The last modification\
+                               time of the entry")
+    accessed = Column(DateTime, server_default=func.now(),
+                      comment="The last time the entry\
+                               was involved in an IDS\
+                               event or scan event")
+
+
+Base = declarative_base(cls=IDSModelBase)
+metadata = Base.metadata
 
 GenderEnum = Enum('mens', 'womens')
 FormatEnum = Enum('tests', 'odis', 't20is')
